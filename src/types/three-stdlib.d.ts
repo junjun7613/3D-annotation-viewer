691f29fc293edfdb37f4f0056672248d66062fe2
junjun7613 <htjk6513khbk@gmail.com>
2024-12-30 23:03:44 +0900

    delete unused modules

--- src/types/three-stdlib.d.ts
+++ src/types/three-stdlib.d.ts
@@ -69,11 +69,11 @@ declare module 'three-stdlib' {
       getAzimuthalAngle(): number;
   
       // EventDispatcher mixins
-      addEventListener(type: string, listener: (event: any) => void): void;
+      addEventListener(type: string, listener: (event: { type: string; target: any }) => void): void;
   
-      hasEventListener(type: string, listener: (event: any) => void): boolean;
+      hasEventListener(type: string, listener: (event: { type: string; target: any }) => void): boolean;
   
-      removeEventListener(type: string, listener: (event: any) => void): void;
+      removeEventListener(type: string, listener: (event: { type: string; target: any }) => void): void;
   
       dispatchEvent(event: { type: string; target: any }): void;
     }
