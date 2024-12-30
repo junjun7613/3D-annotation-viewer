30868c96d5297f31c82ac0bb268a6557502a4db8
junjun7613 <htjk6513khbk@gmail.com>
2024-12-30 22:45:48 +0900

    delete unused modules

---
+++ src/types/CETEIcean.d.ts
@@ -0,0 +1,7 @@
+declare module 'CETEIcean' {
+    export default class CETEI {
+      constructor(base?: string);
+      addBehaviors(behaviors: object): void;
+      getHTML5(xml: string): Promise<HTMLElement>;
+    }
+  }
