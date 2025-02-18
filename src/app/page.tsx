bf35de8b52d0d6c8cd2bbd65e3ae4a64b99c6c2a
junjun7613 <htjk6513khbk@gmail.com>
2025-02-18 16:50:36 +0900

    fix vercel error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,9 +24,6 @@ import {v4 as uuidv4} from 'uuid';
 
 import db from '@/lib/firebase/firebase';
 import { deleteDoc, doc, getDoc, getDocs, updateDoc, collection } from 'firebase/firestore';
-import { info } from 'console';
-//import { info } from 'console';
-//import { info } from 'console';
 
 const Home: NextPage = () => {
   const [user] = useAuthState(auth);
